2230509c6ea6334fc75f5a5088c5963eabb8ab43
Timmy's Macbook Air <timmy@Timmyss-MacBook-Air.local>
2017-10-15 20:45:25 -0700

    deleted extra code

--- windows_logo.py
+++ windows_logo.py
@@ -1,36 +1,3 @@
-# import turtle
-# turtle.shape("triangle")
-# turtle.pencolor("green")
-# turtle.fillcolor("blue")
-# turtle.begin_fill()
-# turtle.forward(50)
-# turtle.right(90)
-# turtle.forward(50)
-# turtle.right(90)
-# turtle.forward(50)
-# turtle.right(90)
-# turtle.forward(100)
-# turtle.right(90)
-# turtle.forward(100)
-# turtle.right(90)
-# turtle.forward(150)
-# turtle.right(90)
-# turtle.forward(150)
-# turtle.right(90)
-# turtle.forward(200)
-# turtle.right(90)
-# turtle.forward(200)
-# turtle.right(90)
-# turtle.forward(250)
-# turtle.right(90)
-# turtle.forward(250)
-# turtle.right(90)
-# turtle.forward(250)
-# turtle.right(90)
-# turtle.forward(125)
-# turtle.right(90)
-# turtle.forward(125)
-# turtle.end_fill()
 import turtle
 import random
 
@@ -55,6 +22,3 @@ for i in range(4):
   turtle.end_fill()
 
 
-
-# turtle.fillcolor(255,0,0)
-# turtle.write("Timmy", align="center", font=("Times",20,"bold")
