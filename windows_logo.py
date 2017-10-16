9140384bb4ea5b650724455e825960298a5634e0
Timmy's Macbook Air <timmy@Timmyss-MacBook-Air.local>
2017-10-15 20:37:37 -0700

    windows logo script

---
+++ windows_logo.py
@@ -0,0 +1,60 @@
+# import turtle
+# turtle.shape("triangle")
+# turtle.pencolor("green")
+# turtle.fillcolor("blue")
+# turtle.begin_fill()
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(50)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(150)
+# turtle.right(90)
+# turtle.forward(150)
+# turtle.right(90)
+# turtle.forward(200)
+# turtle.right(90)
+# turtle.forward(200)
+# turtle.right(90)
+# turtle.forward(250)
+# turtle.right(90)
+# turtle.forward(250)
+# turtle.right(90)
+# turtle.forward(250)
+# turtle.right(90)
+# turtle.forward(125)
+# turtle.right(90)
+# turtle.forward(125)
+# turtle.end_fill()
+import turtle
+import random
+
+for i in range(4):
+  
+  if (i == 0):
+    turtle.fillcolor(255, 210, 10)
+  elif (i == 1):
+    turtle.fillcolor(0, 250, 0)
+  elif (i == 2):
+    turtle.fillcolor("red")
+  elif (i == 3):
+    turtle.fillcolor("blue")
+  turtle.begin_fill()
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.end_fill()
+
+
+
+# turtle.fillcolor(255,0,0)
+# turtle.write("Timmy", align="center", font=("Times",20,"bold")
